589e76b3001dbec511e06b19aef296e12b63da57
= <183406007@qq.com>
2017-09-25 15:41:28 +0800

    '修改viewbug'

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -18,6 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^account/', include('account.urls')),
-    url(r'', include('account.urls', namespace='account', app_name='account')),
+    url(r'^account/', include('account.urls')),
 ]
